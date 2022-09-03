df8f03d9414fe932147be2256fe62d84ebff324b
wangchao <magician.of.technique@gmail.com>
2022-09-03 12:56:06 +0800

    [*] Fix Logo

--- server/pkg/framework/boot/logo.go
+++ server/pkg/framework/boot/logo.go
@@ -4,12 +4,12 @@ import "github.com/fatih/color"
 
 func printLogo() {
 	color.Cyan("")
-	color.Cyan("  ___                   _   ")
-	color.Cyan(" |_ _|_ __   __ _  ___ | |_ ")
-	color.Cyan("  | || '_ \\ / _` |/ _ \\| __|")
-	color.Cyan("  | || | | | (_| | (_) | |_ ")
-	color.Cyan(" |___|_| |_|\\__, |\\___/ \\__|")
-	color.Cyan("            |___/           ")
+	color.Cyan("  ___                       _   ")
+	color.Cyan(" |_ _| _ __    __ _   ___  | |_ ")
+	color.Cyan("  | | | '_ \\  / _` | / _ \\ | __|")
+	color.Cyan("  | | | | | || (_| || (_) || |_ ")
+	color.Cyan(" |___||_| |_| \\__, | \\___/  \\__|")
+	color.Cyan("              |___/             ")
 	color.Cyan(":: Power by jymot ::")
 	color.Cyan("")
 }
